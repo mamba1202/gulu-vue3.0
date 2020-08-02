4c58965f4eab9a549a512189b260c6b92481a1a4
mamba1202 <1612786059@qq.com>
2020-08-02 20:32:58 +0800

    路由及页面跳转

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-
-  <span>count 为{{count}}</span>
-  <br/>
-  <span>object: {{object.n}}</span>
-   <br/>
-  <button @click="add">+1</button>
-  <hr/>
-
-
+  <router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import { ref, reactive } from 'vue'
+<script lang="ts">
+import { ref, provide } from "vue";
+import { router } from "./router";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
+    provide("menuVisible", menuVisible); // set
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false;
+      }
+    });
   },
-
-  setup(){
-
-    //ref 盒子 用value  基本类型
-    //reactive 代理 object是{{n: 100}}代理人  复杂类型 响应式
-    const count  = ref(0)
-    const object = reactive({n: 100})
-
-
-    setTimeout(()=>{
-      count.value +=100
-      object.n += 1000
-    },3000)
-
-
-// 如果return  this访问不到
-    return{
-      count,
-      object
-    }
-  },
-
-  methods:{
-    add(){
-      this.count += 1
-      this.object.n += 1
-    }
-  }
-
-}
+};
 </script>
