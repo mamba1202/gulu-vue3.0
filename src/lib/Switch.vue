b6bece60089ec7e9eab867998a6aefa422906037
mamba1202 <1612786059@qq.com>
2020-08-09 19:30:09 +0800

    添加过渡动画

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -39,11 +39,16 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2/2;
+  transition: left 300ms;
 }
 button.checked {
   background: blue;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+button:focus{
+    outline: none;
 }
 </style>
