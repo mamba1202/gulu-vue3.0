da285979d2e31a7a5434d4b37249fc71419eb3b5
mamba1202 <1612786059@qq.com>
2020-08-16 10:38:57 +0800

    props与attrs区别

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,7 +3,12 @@
     Button 示例
     <h1></h1>
     <div>
-     <Button>你好<Button/>
+     <Button 
+     @click="onClick"
+     @focus="onClick"
+     @mousemove="onClick"
+     size= 'small'
+     >你好<Button/>
     </div>
   </div>
 </template>
@@ -13,5 +18,11 @@ import Button from "../lib/Button.vue";
 
 export default {
   components: { Button },
+  setup(){
+    const onClick  = ()=>{
+      console.log('你好')
+    }
+    return {onClick}
+  }
 };
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,42 @@
 <template>
-    <button>
-        <slot/>
+  <div :size="size">
+    <!-- <button v-bind="$attrs"> -->
+    <button v-bind="rest">
+      <slot />
     </button>
+  </div>
+
 </template>
+<script lang="ts">
+// 属性绑定
+//默认所有的属性都绑定到根元素
+//使用inheritAttrs： false 可以取消默认绑定
+//使用$attrs或者context.attrs获取所有属性
+//使用v-bind="$attrs" 批量绑定属性
+//使用const{size, level,...rest} = context.attrs 将属性分开
+
+// props VS attrs
+// props要先声明才能取值， attrs 不用先声明
+// props不包含事件， attrs包含
+// props 没有声明的属性会跑到attrs里
+// props 支持string以外的类型， atrrs只有string类型
+export default {
+  inheritAttrs: false, //不让其有继承属性
+  props:{
+      size: String
+  },
+  setup(props, context) {
+      console.log({...props})
+      console.log({...context.attrs})
+
+    const { size, ...rest } = context.attrs;
+    return {size, rest}
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>
