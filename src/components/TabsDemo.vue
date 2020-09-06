c22fa9b3326dd04293a25e4f8b4383d5a39a1347
mamba1202 <1612786059@qq.com>
2020-09-06 14:20:47 +0800

    添加css

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,7 +2,7 @@
   <div>Tabs 示例</div>
   <h1>示例一</h1>
 
-  <Tabs>
+  <Tabs selected="导航一">
     <Tab title="导航一">内容一</Tab>
     <Tab title="导航二">内容二</Tab>
   </Tabs>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,14 +1,24 @@
 <template>
-    <div>
-        <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-        <component  v-for="(c,index) in defaults" :is="c" :key="index"></component>
-
-    </div>
+<div class="gulu-tabs">
+  <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav-item" 
+    :class="{selected: t==selected}"
+    v-for="(t,index) in titles" :key="index">{{t}}</div>
+  </div>
+  <div class="gulu-tabs-content">
+    <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+  </div>
+</div>
 </template>
 
 <script>
 import Tab from './Tab.vue'
 export default {
+    props:{
+       selected: {
+          type: String,
+     }
+    },
     setup(props,context){
     console.log({...context.slots.default()})
     const defaults  = context.slots.default()
@@ -32,3 +42,30 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
