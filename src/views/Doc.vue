5ab24f609055ac8a773dee582b542e675cb74363
mamba1202 <1612786059@qq.com>
2020-09-20 16:53:55 +0800

    markdown及边栏处理

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,83 +1,99 @@
 <template>
-  <div class="layout">
-    <Topnav
-      toggleMenuButtonVisible
-      class="nav"
-    />
-    <div class="content">
-      <aside v-if="menuVisible">
-        <h2>文档</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/intro">介绍</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/install">安装</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/get-started">开始使用</router-link>
-          </li>
-        </ol>
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/switch">Switch 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/tabs">Tabs 组件</router-link>
-          </li>
-        </ol>
-      </aside>
-      <main>
-        <router-view />
-      </main>
-    </div>
+<div class="layout">
+  <Topnav toggleMenuButtonVisible class="nav" />
+  <div class="content">
+    <aside v-if="menuVisible">
+      <h2>文档</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/intro">介绍</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/install">安装</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/get-started">开始使用</router-link>
+        </li>
+      </ol>
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
+    <main>
+      <router-view />
+    </main>
   </div>
+</div>
 </template>
+
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
-import { inject, Ref } from "vue";
+import {
+  inject,
+  Ref
+} from "vue";
 export default {
-  components: { Topnav },
+  components: {
+    Topnav
+  },
   setup() {
-    const menuVisible = inject<Ref<boolean>>("menuVisible"); // get
-    return { menuVisible };
+    const menuVisible = inject < Ref < boolean >> ("menuVisible"); // get
+    return {
+      menuVisible
+    };
   },
 };
 </script>
+
 <style lang="scss" scoped>
+$aside-index : 10;
+
 .layout {
   display: flex;
   flex-direction: column;
   height: 100vh;
-  > .nav {
+
+  >.nav {
     flex-shrink: 0;
   }
-  > .content {
+
+  >.content {
     flex-grow: 1;
     padding-top: 60px;
     padding-left: 156px;
+
     @media (max-width: 500px) {
       padding-left: 0;
     }
   }
 }
+
 .content {
   display: flex;
-  > aside {
+
+  >aside {
     flex-shrink: 0;
   }
-  > main {
+
+  >main {
     flex-grow: 1;
     padding: 16px;
+    background: white;
   }
 }
+
 aside {
   background: lightblue;
   width: 150px;
@@ -87,23 +103,28 @@ aside {
   left: 0;
   padding-top: 70px;
   height: 100%;
-  > h2 {
+  z-index: $aside-index;
+
+  >h2 {
     margin-bottom: 4px;
     padding: 0 16px;
   }
-  > ol {
-    > li {
-      a {
+
+  >ol {
+    >li {
+      >a {
         display: block;
         padding: 4px 16px;
         text-decoration: none;
       }
+
       .router-link-active {
-        background-color: #fff;
+        background: white;
       }
     }
   }
 }
+
 main {
   overflow: auto;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,17 +15,17 @@
     <ul>
       <li>
         <svg>
-          <use xlink:href="#icon-Vue"></use>
+          <use xlink:href="#icon-vue"></use>
         </svg>
         <h3>基于 Vue 3</h3>
         <p>骄傲地使用了 Vue 3 Composition API</p>
       </li>
       <li>
         <svg>
-          <use xlink:href="#icon-typescript"></use>
+          <use xlink:href="#icon-ts"></use>
         </svg>
         <h3>基于 TypeScript </h3>
-        <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        <p>源代码采用 TypeScript 书写</p>
       </li>
       <li>
         <svg>
@@ -52,24 +52,41 @@ export default {
 $green: #02bcb0;
 $border-radius: 4px;
 $color: #007974;
+
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
   clip-path: ellipse(80% 60% at 50% 40%);
 }
+
 .features {
   margin: 64px auto;
-  width: 400px;
+  padding: 0 16px;
+
   @media (min-width: 800px) {
     width: 800px;
+
+    >ul {
+      >li {
+        width: 50%;
+      }
+    }
   }
+
   @media (min-width: 1200px) {
     width: 1200px;
+
+    >ul {
+      >li {
+        width: 33.3333%;
+      }
+    }
   }
+
   >ul {
     display: flex;
     flex-wrap: wrap;
+
     >li {
-      width: 400px;
       margin: 16px 0;
       display: grid;
       justify-content: start;
@@ -79,21 +96,25 @@ $color: #007974;
         "icon text";
       grid-template-columns: 80px auto;
       grid-template-rows: 1fr auto;
+
       >svg {
         grid-area: icon;
         width: 64px;
         height: 64px;
       }
+
       >h3 {
         grid-area: title;
         font-size: 28px;
       }
+
       >p {
         grid-area: text
       }
     }
   }
 }
+
 .banner {
   color: $color;
   padding: 100px 0;
@@ -101,8 +122,10 @@ $color: #007974;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
   >.actions {
     padding: 8px 0;
+
     a {
       margin: 0 8px;
       background: $green;
@@ -110,6 +133,7 @@ $color: #007974;
       display: inline-block;
       padding: 8px 24px;
       border-radius: $border-radius;
+
       &:hover {
         text-decoration: none;
       }
