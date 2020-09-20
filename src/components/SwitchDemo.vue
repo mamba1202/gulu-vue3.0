5d28d3c3572741513a90194c8abdb7ccabc25576
mamba1202 <1612786059@qq.com>
2020-09-20 16:42:33 +0800

    删除Switch无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,49 +4,16 @@
 </template>
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
-import Button from "../lib/Button.vue";
+import Demo  from './Demo.vue'
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
-import 'prism.js'
-import 'prism.js/themes/prism.css'
-import Demo from './Demo.vue'
-
-const Prism = (window as any).Prism
-import { ref } from "vue";
 
 export default {
-  components: { Switch, Button, Switch1Demo, Switch2Demo,Demo },
+  components: { Switch1Demo, Switch2Demo,Demo },
   setup() {
-    const bool = ref(true);
-    return { bool ,Switch1Demo,Switch2Demo,Prism};
+    return { Switch1Demo,Switch2Demo};
   },
 };
 </script>
 <style lang="scss" scoped>
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  > h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, "Courier New", Courier, monospace;
-      margin: 0;
-    }
-  }
-}
+</style>
