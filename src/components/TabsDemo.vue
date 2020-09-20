32c20bb4f82a73b8643857fae0c681b76c585c95
mamba1202 <1612786059@qq.com>
2020-09-20 16:49:40 +0800

    封装不同Tabs组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,34 +1,19 @@
 <template>
-  <div>Tabs 示例</div>
-  <h1>示例一</h1>
-
-  <Tabs v-model:selected="x">
-    <Tab title="导航一">内容一</Tab>
-    <Tab title="导航二导航二导航二导航二">内容二</Tab>
-  </Tabs>
+<h1>Tabs 示例</h1>
+<Demo :component="Tabs1Demo" />
 </template>
 
-
-
-
-
 <script lang="ts">
-import Tabs  from '../lib/Tabs.vue'
-import Tab  from '../lib/Tab.vue'
-import { ref } from 'vue'
-
-
-export default{
-  setup(){
-    const x  = ref('导航一')
-    return{
-      x
-    }
+import Demo from './Demo.vue'
+import Tabs1Demo from './Tabs1.demo.vue'
+export default {
+  components: {
+    Demo
   },
-  
-  components:{
-    Tabs,
-    Tab
+  setup() {
+    return {
+      Tabs1Demo
+    }
   }
 }
 </script>
