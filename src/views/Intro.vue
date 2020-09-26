da525cff68577cb958b85d7a19b8449b2971dc1b
mamba1202 <1612786059@qq.com>
2020-09-26 19:28:04 +0800

    解决官网样式

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -3,7 +3,7 @@
     <h1>介绍</h1>
     <p>guli UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
     <p>不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。</p>
-    <p>源代码放在了https://github.com/mamba1202/gulu-vue3.0，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见 README.md。
+    <p>源代码放在了https://github.com/mamba1202/gulu-vue3.0，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见 README.md。</p>
    
   </article>
 </template>
