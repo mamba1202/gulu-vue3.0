b960e2c748d6fd5adfc3c488a6d23baeaa69eb78
mamba1202 <1612786059@qq.com>
2020-09-26 18:37:51 +0800

    查看及隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,49 +1,62 @@
 <template>
-<div class="demo">
-  <h2>{{component.__sourceCodeTitle}}</h2>
-  <div class="demo-component">
-    <component :is="component" />
-  </div>
-  <div class="demo-actions">
-    <Button @click="toggleCode">查看代码</Button>
-  </div>
-  <div class="demo-code" v-if="codeVisible">
-    <pre class="language-html" v-html="html" />
-  </div>
+  <div class="demo">
+    <h2>{{component.__sourceCodeTitle}}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button @click="hideCode"  v-if="codeVisible"> 隐藏代码</Button>
+      <Button @click=" showCode" v-else>查看代码</Button>
 
-</div>
+    </div>
+    <div
+      class="demo-code"
+      v-if="codeVisible"
+    >
+      <pre
+        class="language-html"
+        v-html="html"
+      />
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-import {
-  computed,
-  ref
-} from 'vue';
-const Prism = (window as any).Prism
+import Button from "../lib/Button.vue";
+import "prismjs";
+import "prismjs/themes/prism.css";
+import { computed, ref } from "vue";
+const Prism = (window as any).Prism;
 export default {
   components: {
-    Button
+    Button,
   },
   props: {
-    component: Object
+    component: Object,
   },
   setup(props) {
     const html = computed(() => {
-      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html')
-    })
-    const toggleCode = () => codeVisible.value = !codeVisible.value
-    const codeVisible = ref(false)
+      return Prism.highlight(
+        props.component.__sourceCode,
+        Prism.languages.html,
+        "html"
+      );
+    });
+
+    const showCode = () => (codeVisible.value = true);
+    const hideCode = () => (codeVisible.value = false);
+    const codeVisible = ref(false);
+
     return {
       Prism,
       html,
       codeVisible,
-      toggleCode
-    }
-  }
-}
+      showCode,
+      hideCode,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -53,7 +66,7 @@ $border-color: #d9d9d9;
   border: 1px solid $border-color;
   margin: 16px 0 32px;
 
-  >h2 {
+  > h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
@@ -72,9 +85,9 @@ $border-color: #d9d9d9;
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
 
-    >pre {
+    > pre {
       line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
+      font-family: Consolas, "Courier New", Courier, monospace;
       margin: 0;
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a href="https://github.com/mamba1202/vite-vue3.0">GitHub</a>
+        <a href="https://github.com/mamba1202/gulu-vue3.0">GitHub</a>
         <router-link to="/doc/intro">开始</router-link>
       </p>
     </div>
